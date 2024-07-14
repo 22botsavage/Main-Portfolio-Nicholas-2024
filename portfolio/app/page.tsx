01289105d0f41eff2f65b74c3ab2998bf6cf31f0
22botsavage <nicholasworks2@gmail.com>
2024-07-15 02:28:11 +0700

    Final-Version
    15 July 2024 - Basic Version of the portofolio website, not edited yet still orginial.

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,16 +1,27 @@
 import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
+import RecentProject from "@/components/RecentProject";
+import { navItems } from "@/data";
+import Client from "@/components/Client";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-   <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+   <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
     <div className="max-w-7xl w-full">
-      <FloatingNav navItems={[
-        {name: 'Home', link: '/', icon: <FaHome />}
-      ]} />
+      <FloatingNav navItems={navItems} />
       <Hero />
+      <Grid />
+      <RecentProject />
+      <Client />
+      <Experience />
+      <Approach />
+      <Footer />
     </div>
    </main>
   );
